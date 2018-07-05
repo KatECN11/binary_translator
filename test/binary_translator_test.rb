ead3c13de25094546ed4c08de4f4870290e67752
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-05 13:22:16 -0600

    Adds all tests for Iteration 1

---
+++ test/binary_translator_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/binary_translator'
+
+class BinaryTranslatorTest < Minitest::Test
+
+  def test_it_exists
+    bt = BinaryTranslator.new
+
+    assert_instance_of BinaryTranslator, bt
+  end
+
+  def test_does_it_translate_alpha_to_binary
+    bt = BinaryTranslator.new
+    actual_1 = bt.translate("a")
+
+    assert_equal "000001", actual_1
+
+    actual_2 = bt.translate("z")
+
+    assert_equal "011010", actual_2
+  end
+
+  def test_it_can_translate_more_than_one_character
+    bt = BinaryTranslator.new
+    actual = bt.translate("turing")
+
+    assert_equal "010100010101010010001001001110000111", actual
+  end
+
+  
+
+end
